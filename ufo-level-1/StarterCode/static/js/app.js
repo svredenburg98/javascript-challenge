b3ae2672685bf3e672abc2cb3fae8e94b54e96ee
svredenburg98 <66885962+svredenburg98@users.noreply.github.com>
2020-11-12 09:58:08 -0600

    got the table to filter by date

--- ufo-level-1/StarterCode/static/js/app.js
+++ ufo-level-1/StarterCode/static/js/app.js
@@ -1,6 +1,6 @@
 // from data.js
 var tableData = data;
-
+var button = d3.select("#filter-btn");
 // YOUR CODE HERE!
 //table body
 var tbody = d3.select("tbody");
@@ -23,4 +23,36 @@ tableData.forEach(event => {
      
       cell.text(value);
     });
+  });
+
+  button.on("click", function() {
+
+
+    // remove any data from the table
+    tbody.html("");
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+  
+    // append stats to the table
+    filteredData.forEach(event => {
+        console.log(event);
+      
+        
+        var row = tbody.append("tr");
+      
+        
+        Object.entries(event).forEach(([key, value]) => {
+          console.log(key, value);
+      
+         
+          var cell = row.append("td");
+      
+         
+          cell.text(value);
+        });
+      });
   });
